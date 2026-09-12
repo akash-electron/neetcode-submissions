04cb85dcc3dcd4bb9df5adb80a0aa4ff7f787342
Akash Pandey <128716001+akash-electron@users.noreply.github.com>
2026-09-12 12:12:18 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let e = ""
+        for (let i = 0; i < strs.length; i++) {
+            e += strs[i].length + "#" + strs[i]
+        }
+        return e;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        console.log(str)
+          
+        let a = []
+        let l = "";
+        let i = 0
+        for (i = 0; i < str.length;) {
+            if (str[i] == "#") {
+                let len = Number(l);
+                // console.log(i, " === ", str[i], "l == ", l, " str ", str.slice(i + 1, (Number(l) + i + 1)))
+                a.push(str.slice(i + 1, (Number(l) + i + 1)))
+                i = i + 1 + len;
+                l = "";
+            } else {
+                l += str[i]
+                i++;
+            }
+
+        }
+        return a
+
+    }
+}

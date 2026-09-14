db0caca37244a32982885fb18d28e09e2bad4c8d
Akash Pandey <128716001+akash-electron@users.noreply.github.com>
2026-09-14 22:32:43 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        let r = Array.from({length : 9} , () => new Set())
+        let c = Array.from({length : 9} , () => new Set())
+        let sq = Array.from({length : 9} , () => new Set())
+         
+       for(let i =0 ; i < 9 ; i++ ){
+        for(let j = 0; j < 9; j++){
+            if(board[i][j] == ".") {
+                continue;
+            }
+            let box = Math.floor(i/3)*3 + Math.floor(j/3)
+            if (r[i].has(board[i][j])
+             || c[j].has(board[i][j])
+             || sq[box].has(board[i][j])){
+                return false
+            }
+            r[i].add(board[i][j])
+            c[j].add(board[i][j])
+            sq[box].add(board[i][j])
+        }
+       }
+       return true
+    }
+}

9641638ea0dcb3eb82739bedbb8001dc435e035e
Akash Pandey <128716001+akash-electron@users.noreply.github.com>
2026-09-15 13:53:33 +0530

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        for(let i = 0 ; i < matrix.length ; i++){
+            for(let j = 0 ; j < matrix[i].length; j++){
+
+                if(matrix[i][matrix[i].length-1]  == target) {
+                    console.log("entered equal to loop")
+                        return true
+                } 
+
+                if(matrix[i][matrix[i].length-1]  > target) {
+                    console.log("entered greater than loop")
+                    if(matrix[i][j] == target) {
+                        return true
+                    } else {
+                        continue
+                    }
+                } 
+                
+                if(matrix[i][matrix[i].length-1]  <  target) {
+                    console.log("entered less than loop" ,  i)
+                    break;
+                } 
+            }
+            
+        }
+        return  false
+    }
+}

92a63cde61da1d2ed9eb71d0a0487181c87327cd
Akash Pandey <128716001+akash-electron@users.noreply.github.com>
2026-09-18 22:07:32 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+
+        console.log(s)
+       let ss = s.replace(/[^a-zA-Z0-9]/g,"").toLowerCase()
+       let l=0 , r = ss.length-1
+       for(let i =0; i < ss.length; i++){
+        while(l < r){
+            if(ss[l] !== ss[r]){
+                return false
+            } 
+            l++;
+            r--;
+        }
+       }
+       return true
+    }
+}
